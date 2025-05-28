3b815d352f93ccb873d41c9a935bdcce93a93fc1
Andi Kisare <akisare@outlook.com>
2025-05-28 13:18:53 -0500

    use importlib instead of pkg_resources

--- imgmarker/__init__.py
+++ imgmarker/__init__.py
@@ -4,13 +4,13 @@ __docsurl__ = 'https://imgmarker.readthedocs.io/en/latest/'
 
 import sys
 import os
-import pkg_resources
+from importlib import resources
 
 def resource_path(resource):
     if hasattr(sys,'_MEIPASS'):
         return os.path.join(sys._MEIPASS, resource)
     else: 
-        return pkg_resources.resource_filename(__package__,resource)
+        return str(resources.files(__package__).joinpath(resource))
 
 ICON = resource_path('icon.ico')
 HEART_SOLID = resource_path('heart_solid.ico')
